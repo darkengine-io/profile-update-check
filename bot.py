cc86e6aa5f415950f48f1a3787eaf6c181fab676
Simon McFarlane <illuminatedgeek@gmail.com>
2013-04-11 18:11:07 -0700

    Fetch XML manifest for each project from drupal.org

--- bot.py
+++ bot.py
@@ -8,15 +8,19 @@ except ImportError as e:
     exit()
 
 import re
+import urllib.request
+from xml.dom import minidom
+
+versions = {}
 
 for f in settings.fullPath:
   data = open(f).read().split('\n')
   lines = []
-  versions = {}
 
   for s in data:
     if s[:7] == 'project' and '][version]' in s:
       versions[(s[s.index('[')+1:s.index(']')])] = re.sub('[ "]', '', s[s.index('=')+1:])
       lines.append(s)
 
-  print(f+": "+versions.__str__())
+for project in versions.items():
+  manifest = minidom.parseString(urllib.request.urlopen("http://updates.drupal.org/release-history/"+project[0]+"/7.x").read())
